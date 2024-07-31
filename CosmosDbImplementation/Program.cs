7a8222497d8e83387f935b4cae8f27c557ad2f94
Bohdan Tron <bohdan.tron@wineshipping.com>
2024-07-31 18:30:02 +0300

    added assertion

--- CosmosDbImplementation/Program.cs
+++ CosmosDbImplementation/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using FluentAssertions;
+using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json;
 
 // Database setup
@@ -21,6 +22,35 @@ var operations = new[]
 var response = await container.PatchItemAsync<Sequence>("sample", new PartitionKey("sample"), operations);
 
 Console.WriteLine($"Seq : {response.Resource.Value}, Consumed RU/s = {response.RequestCharge}");
+
+
+// Tests
+// Arrange
+try
+{
+    var tasks = Enumerable.Range(1, 100)
+        .Select(_ => container.PatchItemAsync<Sequence>("sample", new PartitionKey("sample"), operations))
+        .ToList();
+
+    // Act
+    var result = await Task.WhenAll(tasks);
+
+    // Assert
+    var totalCount = result.Length;
+    var distinctCount = result.Distinct().Count();
+
+    totalCount.Should().Be(distinctCount);
+
+    var ordered = result.Select(item => item.Resource.Value).OrderBy(number => number).ToList();
+    for (var i = 0; i < ordered.Count; i++)
+    {
+        ordered[i].Should().Be(i + 2); // Need to use "i + 2" since we already inserted the first value into container
+    }
+}
+finally
+{
+    await database.Database.DeleteAsync();
+}
 
 internal class Sequence
 {
